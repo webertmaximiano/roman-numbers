c02794414c7fd00829d652bad86abcbff03f5df9
Webert Maximiano <webertcoach@gmail.com>
2024-07-31 18:45:41 -0300

    implementing SiteIndex

--- roman-numbers-app/app/Http/Controllers/RomanNumberController.php
+++ roman-numbers-app/app/Http/Controllers/RomanNumberController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\RomanNumberService;
+use Inertia\Inertia;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
 
 class RomanNumberController extends Controller
 {
     public function index() {
-        dd('aqui');
+        return Inertia::render('SiteIndex', [
+                    'canLogin' => Route::has('login'),
+                     'canRegister' => Route::has('register'),
+                     'laravelVersion' => Application::VERSION,
+                     'phpVersion' => PHP_VERSION,
+                 ]);
     }
 }
