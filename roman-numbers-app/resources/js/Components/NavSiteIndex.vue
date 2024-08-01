ca71a721949a858b68c2a5aed4ee45ccc6737309
Webert Maximiano <webertcoach@gmail.com>
2024-08-01 10:33:37 -0300

    create NavSiteIndex

---
+++ roman-numbers-app/resources/js/Components/NavSiteIndex.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    }
+});
+
+</script>
+
+
+<template>
+    <nav v-if="canLogin" class="-mx-3 flex flex-1 justify-end">
+        <Link v-if="$page.props.auth.user" :href="route('dashboard')"
+            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+        Dashboard
+        </Link>
+
+        <template v-else>
+            <Link :href="route('login')"
+                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+            Entrar
+            </Link>
+
+            <Link v-if="canRegister" :href="route('register')"
+                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+            Criar Conta
+            </Link>
+        </template>
+    </nav>
+</template>
--- roman-numbers-app/resources/js/Pages/SiteIndex.vue
+++ roman-numbers-app/resources/js/Pages/SiteIndex.vue
@@ -6,6 +6,7 @@ import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
 import Logo from '@/Components/Logo.vue';
+import NavSiteIndex from '@/Components/NavSiteIndex.vue';
 
 defineProps({
     canLogin: {
@@ -97,24 +98,10 @@ const convertToArabic = () => {
                 <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
                     <!-- Logo  -->
                     <Logo />
-                    <nav v-if="canLogin" class="-mx-3 flex flex-1 justify-end">
-                        <Link v-if="$page.props.auth.user" :href="route('dashboard')"
-                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
-                        Dashboard
-                        </Link>
-
-                        <template v-else>
-                            <Link :href="route('login')"
-                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
-                            Log in
-                            </Link>
-
-                            <Link v-if="canRegister" :href="route('register')"
-                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
-                            Register
-                            </Link>
-                        </template>
-                    </nav>
+                    <NavSiteIndex
+                        :canLogin="canLogin"
+                        :canRegister="canRegister"
+                    />
                 </header>
 
                 <main class="mt-6">
